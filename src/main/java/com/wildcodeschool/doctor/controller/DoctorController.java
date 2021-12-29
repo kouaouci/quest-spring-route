14df2fcefa7ddbe7f6d3f4487830582d5a11b6ef
kouaouci <karimakouaouci2803@gmail.com>
2021-12-29 19:35:40 +0100

    mettre mon code on format

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,7 +1,6 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
-import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,25 +11,20 @@ import org.springframework.web.server.ResponseStatusException;
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/{number}")
-    @ResponseBody
-    public Doctor doctor(@PathVariable int number) {
-    	if (number == 13) {
-    	return new Doctor(13, "Jodie Whittaker");
-    }
-    	if (number< 1 || number >13) {
-    	throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation"+ number);
-    }
-		{
-		throw new ResponseStatusException(HttpStatus.SEE_OTHER);
-			
+	@GetMapping("/doctor/{number}")
+	@ResponseBody
+	public Doctor doctor(@PathVariable int number) {
+		if (number == 13) {
+			return new Doctor(13, "Jodie Whittaker");
 		}
-		
-    	   
-	
-	
-	};
-	
-        
-    }
+		if (number < 1 || number > 13) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation" + number);
+		}
+		{
+			throw new ResponseStatusException(HttpStatus.SEE_OTHER);
 
+		}
+
+	};
+
+}
